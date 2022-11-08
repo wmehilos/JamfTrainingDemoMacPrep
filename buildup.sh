63ba0a4276a0f5d5f489f8c3414d0640bedf8629
William Mehilos <38141943+wmehilos@users.noreply.github.com>
2022-11-08 16:33:04 -0600

    Create buildup.sh
    Need this piece here to test LaunchDaemon creation and loading from a policy

---
+++ buildup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#Testing launchdaemon policy out
+
+/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -title "It's working" -windowType utility -button1 "OK" -description "Jamf Pro created the LD without issue."
